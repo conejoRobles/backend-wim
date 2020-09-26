d3412feb99cee75d1f7bff42f8128686faa3daa3
matias robles <matias.robles1601@gmail.com>
2020-09-26 19:05:59 -0300

    cors y express funcionando

---
+++ src/app.js
@@ -0,0 +1,30 @@
+//imports
+const express = require('express')
+const cors = require('cors')
+
+//inicialización de la app
+var app = express()
+app.use(cors())
+    //CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-cliente-token, x-client-secret, Authorization");
+    next();
+});
+//DEFINICIÓN DE PUERTO
+app.set('port', 3000)
+
+//Pagina de error de servidor
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+//Listen
+app.listen(app.get('port'), () => {
+    console.log('Express started on ' + app.get('port'))
+})
