db23fb0bdc58960413f03437fb619e282d0ed523
matias robles <matias.robles1601@gmail.com>
2020-09-27 20:41:57 -0300

    algunos metodos de empresa

--- src/controllers/empresa.js
+++ src/controllers/empresa.js
@@ -3,9 +3,67 @@ const db = require('../db')
 const empresa = {}
 
 empresa.getAll = (req, res) => {
-    db.ref('/').on('value', (snap) => {
-        let todo = snap.val()
-        console.log(todo)
+    db.ref('Empresas/').on('value', (snap) => {
+        if (snap.val() !== null) {
+            res.json({
+                ok: true,
+                mensaje: 'todas las empresas',
+                payload: snap.val()
+            })
+        }
+    })
+}
+
+empresa.add = (req, res) => {
+    db.ref('Empresa/' + req.id).once('value', (snap) => {
+        if (snap.val() === null) {
+            db.ref('Empresas/' + req.rut).set({
+                rut: req.rut,
+                nombre: req.nombre,
+                pass: req.pass,
+                correo: req.correo,
+                telefono: req.telefono
+            })
+        } else {
+            res.json({
+                ok: false,
+                mensaje: 'La empresa ya existe'
+            })
+        }
+    })
+}
+
+empresa.getByID = (req, res) => {
+    db.ref('Empresa/' + req.rut).once('value', (snap) => {
+        if (snap.val() !== null) {
+            res.json({
+                ok: true,
+                mensaje: 'Empresa encontrada',
+                empresa: snap.val()
+            })
+        }
+    })
+}
+
+empresa.remove = (req, res) => {
+    db.ref('Empresa/' + req.rut).once('value', (snap) => {
+        if (snap.val() !== null) {
+            db.ref('Empresa/' + req.rut).remove((err) => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    res.json({
+                        ok: true,
+                        mensaje: 'Empresa eliminada'
+                    })
+                }
+            })
+        } else {
+            res.json({
+                ok: false,
+                mensaje: 'No se ha encontrado la empresa'
+            })
+        }
     })
 }
 
