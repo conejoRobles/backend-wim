fa59ec452f491651ea8d8cb9d3c55abb40130a4d
matias robles <matias.robles1601@gmail.com>
2020-10-06 15:11:36 -0300

    refactor

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,19 +3,18 @@ const db = require('../db')
 const Login = {}
 
 Login.log = async(req, res) => {
-    let ans
-    return await db.ref('Empresas/').once('value', (snap) => {
+    await db.ref('Empresas/').once('value', (snap) => {
         if (snap.val()) {
             Object.values(snap.val()).map(empresa => {
                 if (empresa.correo == req.body.correo) {
                     if (empresa.pass == req.body.pass) {
-                        res.json({
+                        return res.send({
                             ok: true,
                             empresa,
                             mensaje: 'Inicio de sesión exitoso'
                         })
                     } else {
-                        res.json({
+                        return res.send({
                             ok: false,
                             mensaje: 'Contraseña incorrecta'
                         })
@@ -24,20 +23,22 @@ Login.log = async(req, res) => {
             })
         }
     })
-    return await db.ref('Pasajeros/').once('value', (snap) => {
+    await db.ref('Pasajeros/').once('value', (snap) => {
         if (snap.val()) {
             Object.values(snap.val()).map(pasajero => {
-                if (pasajero.pass == req.body.pass) {
-                    res.json({
-                        ok: true,
-                        pasajero,
-                        mensaje: 'Inicio de sesión exitoso'
-                    })
-                } else {
-                    res.json({
-                        ok: false,
-                        mensaje: 'Contraseña incorrecta'
-                    })
+                if (pasajero.correo == req.body.correo) {
+                    if (pasajero.pass == req.body.pass) {
+                        return res.send({
+                            ok: true,
+                            pasajero,
+                            mensaje: 'Inicio de sesión exitoso'
+                        })
+                    } else {
+                        return res.send({
+                            ok: false,
+                            mensaje: 'Contraseña incorrecta'
+                        })
+                    }
                 }
             })
         }
