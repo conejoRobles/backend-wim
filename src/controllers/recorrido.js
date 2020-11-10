af7df1ff93b5d6ac6a5b94397508a75737b90efe
matias robles <matias.robles1601@gmail.com>
2020-11-10 08:04:12 -0300

    backend con creación de indice

--- src/controllers/recorrido.js
+++ src/controllers/recorrido.js
@@ -2,19 +2,34 @@ const db = require('../db')
 
 const Recorridos = {}
 
-Recorridos.addRecorrido = (req, res) => {
+Recorridos.addRecorrido = async (req, res) => {
 
-	db.ref('Empresas/' + req.body.rut + "/recorridos/" + req.body.id).once('value', (snap) => {
+	await db.ref('Empresas/' + req.body.rut + "/recorridos/" + req.body.id).once('value', (snap) => {
 		if (snap.val() === null) {
-			db.ref('Empresas/' + req.body.rut + '/recorridos/' + req.body.id).set({
-				id: req.body.id,
-				origen: req.body.origen,
-				destino: req.body.destino,
-				precios: req.body.precios
-			})
-			res.json({
-				ok: true,
-				mensaje: 'Su recorrido ha sido agregado con exito'
+			db.ref('Recorridos/' + req.body.origen + '/' + req.body.destino + '/' + req.body.rut).once('value', (snap) => {
+				if (snap.val()) {
+					res.json({
+						ok: false,
+						mensaje: 'el recorrido ya existe'
+					})
+				} else {
+					db.ref('Empresas/' + req.body.rut + '/recorridos/' + req.body.id).set({
+						id: req.body.id,
+						origen: req.body.origen,
+						destino: req.body.destino,
+						precios: req.body.precios
+					})
+					db.ref('Recorridos/' + req.body.origen + '/' + req.body.destino + '/' + req.body.rut).set({
+						origen: req.body.origen,
+						destino: req.body.destino,
+						empresa: req.body.rut,
+						recorrido: req.body.id,
+					})
+					res.json({
+						ok: true,
+						mensaje: 'Su recorrido ha sido agregado con exito'
+					})
+				}
 			})
 		} else {
 			res.json({
@@ -23,7 +38,6 @@ Recorridos.addRecorrido = (req, res) => {
 			})
 		}
 	})
-
 }
 
 Recorridos.editRecorrido = (req, res) => {
@@ -35,6 +49,23 @@ Recorridos.editRecorrido = (req, res) => {
 				origen: req.body.origen,
 				destino: req.body.destino,
 				precios: req.body.precios
+			})
+			if (req.body.origen != req.body.Oldorigen) {
+				if (req.body.destino != req.body.Olddestino) {
+					db.ref('Recorridos/' + req.body.Oldorigen + '/' + req.body.Olddestino + '/' + req.body.rut).remove()
+				} else {
+					db.ref('Recorridos/' + req.body.Oldorigen + '/' + req.body.destino + '/' + req.body.rut).remove()
+				}
+			} else {
+				if (req.body.destino != req.body.Olddestino) {
+					db.ref('Recorridos/' + req.body.Oldorigen + '/' + req.body.Olddestino + '/' + req.body.rut).remove()
+				}
+			}
+			db.ref('Recorridos/' + req.body.origen + '/' + req.body.destino + '/' + req.body.rut).update({
+				origen: req.body.origen,
+				destino: req.body.destino,
+				empresa: req.body.rut,
+				recorrido: req.body.id,
 			})
 			res.json({
 				ok: true,
@@ -54,6 +85,7 @@ Recorridos.removeRecorrido = (req, res) => {
 	db.ref('Empresas/' + req.body.rut + "/recorridos/" + req.body.id).once('value', (snap) => {
 		if (snap.val() != null) {
 			db.ref('Empresas/' + req.body.rut + "/recorridos/" + req.body.id).remove()
+			db.ref('Recorridos/' + req.body.origen + '/' + req.body.destino + '/' + req.body.rut).remove()
 			res.json({
 				ok: true,
 				mensaje: 'Recorrido eliminado con exito'
@@ -66,5 +98,20 @@ Recorridos.removeRecorrido = (req, res) => {
 		}
 	})
 }
+
+Recorridos.searchRecorrido = (req, res) => {
+	db.ref('Recorridos/' + req.body.origen + '/' + req.body.destino).once('value', (snap) => {
+		if (snap.val() != null) {
+			let reco = Object.values(snap.val())
+			reco.map()
+		} else {
+			res.json({
+				ok: false,
+				mensaje: 'No hay nah'
+			})
+		}
+	})
+}
+
 
 module.exports = Recorridos
